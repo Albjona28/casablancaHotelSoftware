2c892ed96571c06e53cb6d1e66b93b19603d62e6
Albjona <Albjona_94>
2023-02-10 00:21:38 +0100

    Contact us

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ const swiper = new Swiper('.swiper', {
 });
 
 //Back to top
-// Get the button:
 let mybutton = document.getElementById("myBtn");
 
 window.onscroll = function () { scrollFunction() };
@@ -46,3 +45,40 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//API Data Staff
+const mainElement = document.querySelector('#staff');
+const getStaff = () => {
+    fetch('https://reqres.in/api/users').then((response) => response.json()).then((data) => {
+        if(data?.data?.length > 0) {
+            renderStaff(data.data)
+        }
+        else {
+            let errorMessage = `
+                <div class='errorMessage'>Unfortunately we can't show you the staff right now. Please come back later!</div>
+            `;
+            mainElement.innerHTML = errorMessage;
+        }
+    });
+}
+
+const renderStaff = (data) => {
+    let allStaff = '';
+    let staffMaxNumberToShow = 5;
+    for(let i = 0; i < staffMaxNumberToShow; i++) {
+        console.log(data[i]);
+        let newStaff = `
+            <div class="staff-card">
+                <img class="card-img-top" src="${data[i].avatar}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${data[i].first_name} ${data[i].last_name}</h5>
+                    <p class="card-text">${data[i].email}</p>
+                </div>
+            </div>
+        `
+        allStaff += newStaff;
+    }
+    mainElement.innerHTML = allStaff;
+}
+
+getStaff(); 
